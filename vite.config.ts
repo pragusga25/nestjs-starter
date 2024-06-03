5978f9cc2f733e1c137ad1e4716212e3a3f39090
Taufik Pragusga <taufik@pragusga.com>
2024-06-03 10:47:36 +0700

    perf(tester): use vitest

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import swc from 'unplugin-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { configDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    coverage: {
+      ...configDefaults.coverage,
+      provider: 'v8',
+      exclude: [
+        '**/node_modules/**',
+        '**/test/**',
+        '**/dist/**',
+        '*.js',
+        '**/*main.ts',
+        '*.module.ts',
+      ],
+      thresholds: {
+        branches: 100,
+        lines: 95,
+        statements: 95,
+        functions: 100,
+        perFile: true,
+      },
+    },
+  },
+  plugins: [
+    tsconfigPaths(),
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+})
