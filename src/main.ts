3e662b6b98cdd4a2ff1abf074c1f8df745d41244
Taufik Pragusga <taufik@pragusga.com>
2024-06-03 07:15:45 +0700

    perf(module): use fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { AppModule } from '@@/app.module'
 import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  )
   await app.listen(3000)
 }
 bootstrap()
